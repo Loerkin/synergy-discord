76a190ea0c6fa1fdc60dd0e5f5f8a00312a05afb
Loerkin <69040714+Loerkin@users.noreply.github.com>
2023-04-06 11:37:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,3 +33,97 @@ client.on('message', async message => {
   
 client.login(process.env.MTA5MzQ1NjI3NTc1NDQ3MTQzNQ.Gf3jMV.8JGqMLdJ3BnKxzgNifPVMHjgnsQLG9e3Wb_IUg)
 });
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(' ');
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      'Musíte být v hlasovém kanálu, abyste mohli přehrát hudbu!'
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+    return message.channel.send(
+      'Potřebuji oprávnění k připojení se k hlasovému kanálu a mluvení v něm!'
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+    title: songInfo.title,
+    url: songInfo.video_url,
+  };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true,
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      const connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`**${song.title}** byla přidána do fronty!`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      'Musíte být v hlasovém kanálu, abyste mohli přeskočit píseň!'
+    );
+  if (!serverQueue)
+    return message.channel.send('Nemohu přeskočit píseň, protože fronta je prázdná!');
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+if (!message.member.voice.channel)
+return message.channel.send(
+'Musíte být v hlasovém kanálu, abyste mohli zastavit přehrávání hudby!'
+);
+
+if (!serverQueue)
+return message.channel.send('Nemohu zastavit přehrávání, protože fronta je prázdná!');
+
+serverQueue.songs = [];
+serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+const serverQueue = queue.get(guild.id);
+if (!song) {
+serverQueue.voiceChannel.leave();
+queue.delete(guild.id);
+return;
+}
+
+const dispatcher = serverQueue.connection
+.play(ytdl(song.url))
+.on('finish', () => {
+serverQueue.songs.shift();
+play(guild, serverQueue.songs[0]);
+})
+.on('error', error => {
+console.error(error);
+});
+dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+serverQueue.textChannel.send(Začíná přehrát: **${song.title}**);
+}
