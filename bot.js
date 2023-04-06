1bfbbf4dd3955a89ac7f5a0979f582d23922a395
Loerkin <69040714+Loerkin@users.noreply.github.com>
2023-04-06 17:30:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+const config = require('./config.json');
 
 const queue = new Map();
 
